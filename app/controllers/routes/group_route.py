f9b5faf5b7affc93e03587920efa61e81a683bb4
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-08-22 23:40:08 -0300

    feat: rota de adição de membros

--- app/controllers/routes/group_route.py
+++ app/controllers/routes/group_route.py
@@ -1,6 +1,7 @@
 from app.db_config import db
 from app.models.tables.group import Group
 from app.models.tables.user_has_group import user_has_group
+from app.models.tables.product_has_group import product_has_group
 from flask import jsonify, request, Blueprint
 from app.models.schemas.group_schema import GroupSchema
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -38,3 +39,59 @@ def create_group():
                     'status': 'error',
                     'message': 'An error has occurred!'
                 }), 500
+
+
+@group_route.route('/api/v1/add_new_member', methods=["POST"])
+def add_new_member():
+    if request.method == 'POST':
+        try:
+            body = request.get_json()
+            user_id = body['user_id']
+            group_id = body['group_id']
+
+            user_has_group_entry = {
+                'user_id': user_id,
+                'group_id': group_id 
+            }
+            db.session.execute(user_has_group.insert().values(user_has_group_entry))
+            db.session.commit()
+
+            return jsonify({
+                'status': 'ok',
+                'message': 'Novo membro adicionado com sucesso!'
+            }), 201
+        
+        except Exception as error:
+            print(error)
+            return jsonify({
+                    'status': 'error',
+                    'message': 'An error has occurred!'
+                }), 500
+        
+
+# @group_route.route('/api/v1/add_new_product', methods=["POST"])
+# def add_new_member():
+#     if request.method == 'POST':
+#         try:
+#             body = request.get_json()
+#             product_id = body['product_id']
+#             group_id = body['group_id']
+
+#             product_has_group_entry = {
+#                 'product_id': product_id,
+#                 'group_id': group_id 
+#             }
+#             db.session.execute(product_has_group.insert().values(product_has_group_entry))
+#             db.session.commit()
+
+#             return jsonify({
+#                 'status': 'ok',
+#                 'message': 'Novo mebro adicionado com sucesso!'
+#             }), 201
+        
+#         except Exception as error:
+#             print(error)
+#             return jsonify({
+#                     'status': 'error',
+#                     'message': 'An error has occurred!'
+#                 }), 500
